5448a14ccc2a1675ec05feaa8b4a0a83c58efcc8
Jorge Lee <dtrooper@gmail.com>
2011-11-17 01:44:32 -0500

    Working solution that passes all tests. Some optimization and clean up to be done

--- Luhny.scala
+++ Luhny.scala
@@ -1,32 +1,77 @@
+import scala.collection.mutable.MutableList
+
 object Luhny {
-  def luhnCheck(cc:String):Boolean = {
-    def asInt(c:Char):Int = c - '0'
-    def luhnCheckImpl(sum:Int, cc:String, m:Int):Int = {
-      if (cc == "")
-        sum
-      else 
-        luhnCheckImpl(sum + (asInt(cc.head) * m).toString.foldLeft(0)((r,c) => r + asInt(c)), cc.tail, m % 2 + 1)
-    }
+	private val valueMap = Map(0 -> 0, 1 -> 2, 2 -> 4, 3 -> 6, 4 -> 8, 5 -> 1, 6 -> 3, 7 -> 5, 8 -> 7, 9 -> 9)
 
-    return luhnCheckImpl(0, cc.reverse.replaceAll("[\\s-]", ""), 1) % 10 == 0
-  }
+  def mask(cc:String):String = {
 
+		def maskImpl(cc:String):String = {
+			
+			def luhn(start:Int, end:Int, sum:Int, cc:List[Boolean], values:List[Int]) : List[Boolean] = {
+				val newRanges = {
+					if (sum % 10 == 0)
+						cc.zipWithIndex map{case (e, i) => if (start <= i && i <= end) true else e} toList
+					else
+						cc
+				}
+				
+				if (end - start >= 14) {
+					luhn(start + 1, end, sum - values(start), newRanges, values)
+				} else if (end <= cc.size - 3) {
+					luhn(start, end + 2, sum + values(end + 1) + values(end + 2), newRanges, values)
+				} else {
+					newRanges
+				}
+			}
+			
+			def emptyList(size:Int) :List[Boolean] = 0 until size map {e => false} toList
 
-  def maskCc(cc:String):String = {
-    if (14.to(16).contains(cc.size) && luhnCheck(cc)) 
-      cc.map{s => 
-        if (s.toString.matches("\\d"))
-          'X' 
-        else 
-          s
-      }
-    else 
-      cc
+			// convert string to list of ints
+			val baseValueList = cc map {c => (c - '0').toInt} toList;
+			
+			// list with even indexes doubled
+			val evens = baseValueList.zipWithIndex map {case (e,i) => if (i % 2 == 0) valueMap(e) else e} toList;
+			
+			// list with odd indexes doubled
+			val odds = baseValueList.zipWithIndex map {case (e,i) => if (i % 2 == 0) e else valueMap(e)} toList;
+			
+			// get all possible ccs with list of evens
+			val charMap1 = luhn(0, 13, evens.take(14).sum, emptyList(cc.size), evens)
+			
+			// same with list of odds
+			val charMap2 = {
+				if (cc.size >= 15)
+					luhn(0, 14, odds.take(15).sum, emptyList(cc.size), odds) 
+				else
+					emptyList(cc.size)
+			}
+			
+			cc.zipWithIndex map{case (c, i) => if (charMap1(i) || charMap2(i)) 'X' else c} mkString;
+		}
+	
+		if (cc.size < 14)
+			return cc
+	
+		val cleanStr = new StringBuilder()
+		val digits = new MutableList[Int]()
+		
+		for (i <- 0 until cc.size) {
+			if ('0' <= cc.charAt(i) && cc.charAt(i) <= '9'){
+				cleanStr.append(cc(i))
+				digits += i
+			}
+		}
+		
+		if (cleanStr.size >= 14) {
+			val masked = new StringBuilder(cc)
+			maskImpl(cleanStr.toString).zipWithIndex map {case (c, i) => masked.setCharAt(digits(i), c)}
+			masked toString
+		} else cc
   }
 
   def main(args:Array[String]) {
     for (ln <- io.Source.stdin.getLines)
-      print(maskCc(ln) + "\n")
+      print(mask(ln) + "\n")
   }
 }
 
